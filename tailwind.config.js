bb0c77622369104927a289b2d835bbea32fbc363
mibu7 <romerocabelloj0@gmail.com>
2025-06-18 17:25:55 +0200

    💄 Rediseño completo con Tailwind: fondo oscuro, botón rojo, secciones separadas y estilos mejorados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
